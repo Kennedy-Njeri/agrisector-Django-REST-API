b693ed215f0cb59d54bb22af92a321701b2b075b
Kennedy Njeri <mistakenz123@gmail.com>
2019-04-11 15:59:18 +0300

    fix

--- agriculture/settings.py
+++ agriculture/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -121,8 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
--- agriculture/wsgi.py
+++ agriculture/wsgi.py
@@ -12,6 +12,12 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
+
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agriculture.settings')
 
 application = get_wsgi_application()
+
+
+
